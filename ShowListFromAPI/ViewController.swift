fbf8bfe13fc45730991d636dd0a7f0742a6e8823
Livnoor Brar <livnoorbrar@gmail.com>
2017-09-12 17:53:25 +0530

    Initial commit

---
+++ ShowListFromAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowListFromAPI
+//
+//  Created by Livnoor Brar on 12/09/17.
+//  Copyright © 2017 Livnoor Brar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
