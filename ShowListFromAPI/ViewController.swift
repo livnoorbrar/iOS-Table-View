3d77619f35e7b6ecbc141ffbfb20562ab418ad93
Livnoor Brar <livnoorbrar@gmail.com>
2017-09-14 17:12:25 +0530

    Add title to main app

--- ShowListFromAPI/ViewController.swift
+++ ShowListFromAPI/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "My Training App"
         parseData()
     }
     
