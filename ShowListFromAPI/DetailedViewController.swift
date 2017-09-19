7459a17869cb1aaefa671cec61e845b000c727d6
Livnoor Brar <livnoorbrar@gmail.com>
2017-09-19 13:05:06 +0530

    Add scroll view to detailed view along with textview

--- ShowListFromAPI/DetailedViewController.swift
+++ ShowListFromAPI/DetailedViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class DetailedViewController: UIViewController {
 
+    @IBOutlet weak var holdingView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var DescriptionText: UITextView!
+    
     var avatarImage: UIImage?
     var avatarName : String = "Detailed View"
-    
+
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
         avatarImageView.image = avatarImage
         self.title = avatarName
+        DescriptionText.text = "CocoaPods is a dependency management tool for objective-C projects similar to what Maven's for Java projects, which is written in Ruby and is made of several Ruby Gems. The idea is that once you add the third party libraries to your project you'll no longer need to check if there's any newer versions. CocoaPods will handle that for you. It also makes reversing back to a certain version of the library super easy."
+        print(scrollView.contentSize)
+        scrollView.contentSize = CGSize(width: view.bounds.width, height: (holdingView.frame.height))
+        print(scrollView.contentSize)
+        view.layoutIfNeeded()
+        print(scrollView.contentSize)
+        
     }
 
     override func didReceiveMemoryWarning() {
